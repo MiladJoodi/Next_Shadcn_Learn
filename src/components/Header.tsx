6e0c7b068baea9bf2dc345d50a51283b0e302c49
Milad Joodi <winbeta.com@gmail.com>
2024-07-04 17:23:08 +0330

    Update Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div className="flex gap-4 border-r min-h-screen p-4 border-b bg-red-100">
+            Header
+        </div>
+    );
+}
+
+export default Header;
