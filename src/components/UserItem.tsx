badd797d2ce95ea2d1a701335c124e3afce6a264
Milad Joodi <winbeta.com@gmail.com>
2024-07-04 15:13:55 +0330

    change favicon

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,7 +1,15 @@
 const UserItem = () => {
     return (
         <div>
-            UserItem
+
+            <div className="avatar rounded-full h-12 w-12 bg-emerald-500 text-white font-[700] flex items-center justify-center">
+                <p>MJ</p>
+            </div>
+
+            <div>
+                <p>Milad Joodi</p>
+                <p>MiladJoodi1@gmail.com</p>
+            </div>
         </div>
     );
 }
