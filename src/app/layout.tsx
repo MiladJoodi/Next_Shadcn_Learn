eca0c4ccf0df7f3f25631f77a8c559fec8d07c54
Milad Joodi <winbeta.com@gmail.com>
2024-07-04 17:23:16 +0330

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} flex items-start justify-between`}>
         <Sidebar />
         <main className="w-full h-full">
+          <Header />
           {children}
         </main>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      Homepage
       <Button variant="outline">Click</Button>
     </div>
   );
