9425da0fdf2f59fcf1912f422448be7589b92591
Milad Joodi <winbeta.com@gmail.com>
2024-07-04 19:10:17 +0330

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { Button } from "@/components/ui/button"
 import { CommandDemo } from "./Command";
+import { BellIcon } from "lucide-react"
 
 const Header = () => {
     return (
-        <div className="flex gap-4 p-4 border-b">
+        <div className="grid grid-cols-2 gap-4 p-4 border-b">
             <CommandDemo />
+
+            <div className="flex items-center justify-end">
+                <Button variant="outline" size="icon">
+                    <BellIcon className="h-4 w-4" />
+                </Button>
+            </div>
         </div>
     );
 }
