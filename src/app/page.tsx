ae1f8632368fc0dbaabf0d4bb057fb61409ee5c3
Milad Joodi <winbeta.com@gmail.com>
2024-07-05 20:58:13 +0330

    UP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Calendar from "@/components/Cards/Calendar";
 import General from "@/components/Cards/General";
 import Lines from "@/components/Cards/Lines";
+import { TableDemo } from "@/components/Cards/TableComponent";
 import { Card } from "@/components/ui/card";
 // https://github.com/guillaumeduhan/nextjs-shadcn-dashboard/blob/main/src/components/Sidebar.tsx
 
@@ -14,10 +15,12 @@ export default function Home() {
           <Calendar />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-[32px]">
+      <div className="grid grid-cols-3 gap-[32px] h-[300px] overflow-hidden">
           <Lines />
-          <Card className="h-[300px]">Hello world</Card>
-          <Card className="h-[300px]">Hello world</Card>
+          <Card className="p-[32px]">
+            <TableDemo />
+          </Card>
+          <Card>Hello world</Card>
         </div>
     </div>
   );
