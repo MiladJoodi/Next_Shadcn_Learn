a85fd3a35977c86b2ad135364c83854d46948e55
Milad Joodi <winbeta.com@gmail.com>
2024-07-05 13:24:49 +0330

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
+import Calendar from "@/components/Cards/Calendar";
 import General from "@/components/Cards/General";
+import { Card } from "@/components/ui/card";
 // https://github.com/guillaumeduhan/nextjs-shadcn-dashboard/blob/main/src/components/Sidebar.tsx
 
 export default function Home() {
   return (
-    <div>
-      <div className="grid grid-cols-2">
+    <div className="grid gap-[32px]">
+      <div className="grid grid-cols-2 gap-[32px]">
         <General />
+        <div className="grid gap-[32px]">
+          <Calendar />
+          <Calendar />
+        </div>
       </div>
+      <div className="grid grid-cols-3 gap-[32px]">
+          <Card className="h-[300px]">Hello world</Card>
+          <Card className="h-[300px]">Hello world</Card>
+          <Card className="h-[300px]">Hello world</Card>
+        </div>
     </div>
   );
 }
