8370576f2da2e22db4ce4e58dfe852a2b3e4e7b1
Milad Joodi <winbeta.com@gmail.com>
2024-07-06 15:46:51 +0330

    up

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,96 +1,116 @@
-"use client"
-import { Bell, Cookie, CreditCard, Inbox, MessageSquare, Settings, User } from "lucide-react";
-import UserItem from "./UserItem";
+"use client";
 import {
-    Command,
-    CommandDialog,
-    CommandEmpty,
-    CommandGroup,
-    CommandInput,
-    CommandItem,
-    CommandList,
-    CommandSeparator,
-    CommandShortcut,
-  } from "@/components/ui/command"
-  
-const Sidebar = () => {
+  Bell,
+  Cookie,
+  CreditCard,
+  Inbox,
+  MessageSquare,
+  Settings,
+  User,
+} from "lucide-react";
+// import UserItem from "./UserItem";
+import UserItem from "useritem";
 
-    const menuList = [
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+const Sidebar = () => {
+  const menuList = [
+    {
+      group: "General",
+      items: [
         {
-            group: "General",
-            items: [
-                {
-                    link: "/",
-                    icon: <User />,
-                    text: "Profile"
-                },
-                {
-                    link: "/",
-                    icon: <Inbox />,
-                    text: "Inbox"
-                },
-                {
-                    link: "/",
-                    icon: <CreditCard />,
-                    text: "Billing"
-                },
-                {
-                    link: "/",
-                    icon: <Bell />,
-                    text: "Notification"
-                },
-            ]
+          link: "/",
+          icon: <User />,
+          text: "Profile",
         },
         {
-            group: "Settings",
-            items: [
-                {
-                    link: "/",
-                    icon: <Settings />,
-                    text: "General Settings"
-                },
-                {
-                    link: "/",
-                    icon: <Cookie />,
-                    text: "Privacy"
-                },
-                {
-                    link: "/",
-                    icon: <MessageSquare />,
-                    text: "Log"
-                },
-            ]
-        }
-    ]
-    
-    return (
-        <div className="fixed flex flex-col gap-4 w-[300px] min-w-[300px] p-4 min-h-screen">
-            {/* User Item */}
-            <div>
-                <UserItem />
-            </div>
+          link: "/",
+          icon: <Inbox />,
+          text: "Inbox",
+        },
+        {
+          link: "/",
+          icon: <CreditCard />,
+          text: "Billing",
+        },
+        {
+          link: "/",
+          icon: <Bell />,
+          text: "Notification",
+        },
+      ],
+    },
+    {
+      group: "Settings",
+      items: [
+        {
+          link: "/",
+          icon: <Settings />,
+          text: "General Settings",
+        },
+        {
+          link: "/",
+          icon: <Cookie />,
+          text: "Privacy",
+        },
+        {
+          link: "/",
+          icon: <MessageSquare />,
+          text: "Log",
+        },
+      ],
+    },
+  ];
 
-            {/* Menu */}
-            <div className="grow">
-                <Command style={{overflow: "visible"}}>
-                    <CommandList style={{overflow: "visible"}}>
-                        {menuList.map((menu:any, key:number)=> (
-                            <CommandGroup key={key} heading={menu.group}>
-                            {menu.items.map((option: any, optionKey: number)=> (
-                                <CommandItem key={optionKey} className="flex gap-2 cursor-pointer">
-                                    {option.icon}
-                                    {option.text}
-                                    </CommandItem>
-                            ))}
-                        </CommandGroup>
-                        ))}
-                    </CommandList>
-                </Command>
-            </div>
+  return (
+    <div className="fixed flex flex-col gap-4 w-[300px] min-w-[300px] p-4 min-h-screen">
+      {/* User Item */}
+      <div>
+        {/* <UserItem /> */}
+        <UserItem
+          title="John Doe"
+          description="johndoe@mail.com"
+          shadow={false}
+           style={{
+            border: '1px solid red'
+           }}
+        />
+      </div>
 
-            <div>Setting / Notification</div>
-        </div>
-    );
-}
+      {/* Menu */}
+      <div className="grow">
+        <Command style={{ overflow: "visible" }}>
+          <CommandList style={{ overflow: "visible" }}>
+            {menuList.map((menu: any, key: number) => (
+              <CommandGroup key={key} heading={menu.group}>
+                {menu.items.map((option: any, optionKey: number) => (
+                  <CommandItem
+                    key={optionKey}
+                    className="flex gap-2 cursor-pointer"
+                  >
+                    {option.icon}
+                    {option.text}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            ))}
+          </CommandList>
+        </Command>
+      </div>
+
+      <div>Setting / Notification</div>
+    </div>
+  );
+};
 
 export default Sidebar;
